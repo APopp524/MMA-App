3c7a3e21a5847617da24cf2f0df5abec34420d37
APopp524 <apopp524@gmail.com>
2021-12-07 11:03:38 -0600

    version 2 init

--- src/server/routes/schedule.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/schedule';
-const router = express.Router();
-
-router.get('/schedule', controller.getSchedule);
-router.get('/event', controller.getEvent);
-
-export = router;
